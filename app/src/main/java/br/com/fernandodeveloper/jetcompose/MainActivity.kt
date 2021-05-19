0ebb4b99b39024e42374bee5e2ee01cf3e7e1abe
Fernando Souza <fernandosouzaconceicao@gmail.com>
2021-05-19 10:41:41 -0300

    animate list and colors, clickable etc.

--- app/src/main/java/br/com/fernandodeveloper/jetcompose/MainActivity.kt
+++ app/src/main/java/br/com/fernandodeveloper/jetcompose/MainActivity.kt
@@ -3,15 +3,21 @@ package br.com.fernandodeveloper.jetcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Divider
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.fernandodeveloper.jetcompose.ui.theme.JetComposeBasicsTheme
@@ -29,10 +35,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!", modifier = modifier
-            .padding(16.dp)
+    var isSelected by remember {
+        mutableStateOf(false)
+    }
+
+    val targetColour by animateColorAsState(
+        targetValue = if (isSelected) Color.Green else Color.Transparent,
+        animationSpec = tween(durationMillis = 3000)
     )
+
+
+    Surface(color = targetColour) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+                .padding(16.dp)
+                .clickable {
+                    isSelected = !isSelected
+                },
+        )
+    }
 }
 
 @Composable
@@ -51,26 +73,28 @@ fun Counter(count: Int, updateCount: (Int) -> Unit) {
 
 @Composable
 fun MyScreenContent(listOfNames: List<String> = listOf("Android", "Fernando", "You")) {
-    var counter = 0
-    /*var counter by remember {
+    var counter by remember {
         mutableStateOf(0)
-    }*/
-    Column {
-        for (name in listOfNames) {
-            Greeting(name = name)
-            Divider()
-        }
+    }
+    Column(modifier = Modifier.fillMaxHeight()) {
+        NameList(names = List(1000) { "Hello Android $it" }, modifier = Modifier.weight(1f))
         Counter(count = counter,
             updateCount = { newCount ->
                 counter = newCount
             })
 
-        Divider()
-
-
-
         if (counter > 5) {
             Text(text = "I love count!")
+        }
+    }
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) {
+            Greeting(name = it)
+            Divider()
         }
     }
 }
