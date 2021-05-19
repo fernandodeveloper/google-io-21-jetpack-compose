47333937d45109463b21246447c3c50b0ef08087
Fernando Souza <fernandosouzaconceicao@gmail.com>
2021-05-19 10:18:49 -0300

    remember and mutablestateof

--- app/src/main/java/br/com/fernandodeveloper/jetcompose/MainActivity.kt
+++ app/src/main/java/br/com/fernandodeveloper/jetcompose/MainActivity.kt
@@ -5,9 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,11 +43,34 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(onClick = { updateCount(count + 1) }) {
+        Text(text = "I've been clicked $count times.")
+    }
+}
+
+@Composable
 fun MyScreenContent(listOfNames: List<String> = listOf("Android", "Fernando", "You")) {
-    Column() {
+    var counter = 0
+    /*var counter by remember {
+        mutableStateOf(0)
+    }*/
+    Column {
         for (name in listOfNames) {
             Greeting(name = name)
             Divider()
+        }
+        Counter(count = counter,
+            updateCount = { newCount ->
+                counter = newCount
+            })
+
+        Divider()
+
+
+
+        if (counter > 5) {
+            Text(text = "I love count!")
         }
     }
 }
