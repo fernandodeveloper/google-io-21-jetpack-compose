65e8a28591eb31f383e3cee9e0685947a544c8e5
Fernando Souza <fernandosouzaconceicao@gmail.com>
2021-05-19 09:43:55 -0300

    more flexible

--- app/src/main/java/br/com/fernandodeveloper/jetcompose/MainActivity.kt
+++ app/src/main/java/br/com/fernandodeveloper/jetcompose/MainActivity.kt
@@ -3,19 +3,23 @@ package br.com.fernandodeveloper.jetcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.fernandodeveloper.jetcompose.ui.theme.JetComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
+            MyApp() {
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
                 }
@@ -25,14 +29,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Yellow) {
+        Text(
+            text = "Hello $name!", modifier = modifier
+                .padding(16.dp)
+                .background(Color.Magenta)
+        )
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    JetComposeBasicsTheme {
+        content()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetComposeBasicsTheme {
-        Greeting("Android")
+    MyApp() {
+        Surface(color = MaterialTheme.colors.background) {
+            Greeting("Android")
+        }
     }
 }
