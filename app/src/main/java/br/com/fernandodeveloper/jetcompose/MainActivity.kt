44227d26077e334bda1aa80f550a5bb938aa0e90
Fernando Souza <fernandosouzaconceicao@gmail.com>
2021-05-19 09:49:44 -0300

    more flexible, with for

--- app/src/main/java/br/com/fernandodeveloper/jetcompose/MainActivity.kt
+++ app/src/main/java/br/com/fernandodeveloper/jetcompose/MainActivity.kt
@@ -3,14 +3,12 @@ package br.com.fernandodeveloper.jetcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.fernandodeveloper.jetcompose.ui.theme.JetComposeBasicsTheme
@@ -20,9 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp() {
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyScreenContent()
             }
         }
     }
@@ -30,13 +26,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Yellow) {
-        Text(
-            text = "Hello $name!", modifier = modifier
-                .padding(16.dp)
-                .background(Color.Magenta)
-        )
-    }
+    Text(
+        text = "Hello $name!", modifier = modifier
+            .padding(16.dp)
+    )
 }
 
 @Composable
@@ -46,12 +39,21 @@ fun MyApp(content: @Composable () -> Unit) {
     }
 }
 
+@Composable
+fun MyScreenContent(listOfNames: List<String> = listOf("Android", "Fernando", "You")) {
+    Column() {
+        for (name in listOfNames) {
+            Greeting(name = name)
+            Divider()
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApp() {
-        Surface(color = MaterialTheme.colors.background) {
-            Greeting("Android")
-        }
+        MyScreenContent()
+
     }
 }
